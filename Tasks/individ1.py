d6ba09a36e8a79739c754b0764736b3a9c8f866b
Dmitry-15 <dimaggio.dima@mail.ru>
2022-05-16 11:36:51 +0300

    Задания

--- Tasks/individ1.py
+++ Tasks/individ1.py
@@ -14,54 +14,50 @@ CONST_ABSOLUTE = 1e-07
 
 
 def value_x(x=0.35):
-    return math.log(math.sqrt((1 + 0.35) / (1 - 0.35)))
+    return math.log(math.sqrt((1 + x) / (1 - x)))
 
 
-def value_y(x=1):
-    return math.pow(3, x)
+def value_y(x=1.2):
+    return 1/(2 + x)
 
 
-def sum_1():
-    x = 0.35
-    previous = 0
+def sum_1(x):
     s = 0
     n = 0
-    curr = (math.pow(x, (2 * n - 1)) / (2 * n - 1))
-    s += curr
-    n += 1
-    while abs(curr - previous) > CONST_ABSOLUTE:
-        previous = curr
+    curr = 0
+    while True:
+        previous = (math.pow(x, (2 * n - 1)) / (2 * n - 1))
+        n += 1
+        if abs(curr - previous) < CONST_ABSOLUTE:
+            break
         curr = (math.pow(x, (2 * n - 1)) / (2 * n - 1))
-        n += 1
         s += curr
     return s
 
 
-def sum_2():
-    x = 1
-    previous = 0
+def sum_2(x):
     s = 0
     n = 0
-    curr = (math.pow(x, n) * math.pow(math.log(3), n)) / math.factorial(n)
-    s += curr
-    n += 1
-    while abs(curr - previous) > CONST_ABSOLUTE:
-        previous = curr
-        curr = (math.pow(x, n) * math.pow(math.log(3), n)) / math.factorial(n)
+    curr = 0
+    while True:
+        previous = (math.pow(x, n) * math.pow(math.log(3), n)) / math.factorial(n)
         n += 1
+        if abs(curr - previous) < CONST_ABSOLUTE:
+            break
+        curr = (math.pow(x, n) * math.pow(math.log(3), n)) / math.factorial(n)
         s += curr
     return s
 
 
-def compare(x, y):
-    result = x - y
+def compare(first, second, x):
+    result = first(x) - second(x)
     print(f"Результат сравнения {result}")
 
 
 if __name__ == '__main__':
-    th1 = Thread(target=compare(sum_1(), value_x()))
+    th1 = Thread(target=compare(sum_1, value_x, 0.35))
+    th2 = Thread(target=compare(sum_2, value_y, 1.2))
     th1.start()
-    th2 = Thread(target=compare(sum_2(), value_y()))
     th2.start()
     th1.join()
     th2.join()
